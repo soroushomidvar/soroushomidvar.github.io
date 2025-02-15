dca653e94165cd806b76f4cba580cd9517862397
soroushomidvar <11209499+soroushomidvar@users.noreply.github.com>
2025-02-15 21:18:38 +0000

    Deploying to gh-pages from @ soroushomidvar/soroushomidvar.github.io@281b3f8ae1725f0e5e21cd3deb982c2afca6dfc5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-awards",
+          title: "Awards",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/awards.html";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
