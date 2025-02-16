5c9bd0f295cbce6de73155b8edb57b7f2d4f89c7
soroushomidvar <11209499+soroushomidvar@users.noreply.github.com>
2025-02-16 04:14:35 +0000

    Deploying to gh-pages from @ soroushomidvar/soroushomidvar.github.io@670b3af5322f128e890a15ae008ea3ee21f5d2ab 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -48,6 +48,17 @@ ninja.data = [{
           window.location.href = "/posts/2020-11-30-latex-v1/";
         
       },
+    },{id: "post-a-post-with-formatting-and-links",
+      
+        title: "a post with formatting and links",
+      
+      description: "march &amp; april, looking forward to summer",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2015/formatting-and-links/";
+        
+      },
     },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
